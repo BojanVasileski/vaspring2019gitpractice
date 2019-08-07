2e864f5813b2d6087d72070701ce9e0cd67b7c81
CybertekSchool <github@cybertekschool.com>
2019-08-06 21:29:35 -0400

    changed numbers

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -3,9 +3,9 @@ public class LoginTests {
 
     public static void main(String[] args) {
         System.out.println("line 1");
-        System.out.println("line 1");
-        System.out.println("line 1");
-        System.out.println("line 1");
+        System.out.println("line 2");
+        System.out.println("line 3");
+        System.out.println("line 4");
 
     }
 
